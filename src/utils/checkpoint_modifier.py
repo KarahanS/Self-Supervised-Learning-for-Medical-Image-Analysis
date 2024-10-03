01471039cb993370fbff12272849e25188fff0f5
KarahanS <karabatu-2000@hotmail.com>
2024-10-03 19:40:45 +0200

    add dino

--- src/utils/checkpoint_modifier.py
+++ src/utils/checkpoint_modifier.py
@@ -3,6 +3,8 @@ from omegaconf import DictConfig, OmegaConf
 from src.ssl.methods import METHODS
 from src.args.pretrain import parse_cfg, _N_CLASSES_MEDMNIST
 
+# Keep only the backbones - remove the rest (classifier, projector, etc.)
+
 class CheckpointModifier:
     def __init__(self, cfg: DictConfig):
         self.cfg = cfg
@@ -41,7 +43,7 @@ class CheckpointModifier:
         ckpt = torch.load(ckpt_path)
         state_dict = ckpt['state_dict']
 
-        if self.cfg.method in ["simclr", "byol"]:
+        if self.cfg.method in ["simclr", "byol", "dino"]:
             self.remove_keys_starting_with(state_dict, 'online_classifier')
             torch.save(ckpt, ckpt_path)
             print(f"Modified checkpoint saved to: {ckpt_path}")
@@ -58,21 +60,29 @@ class CheckpointModifier:
             # Found keys that are not in the model state dict but in the checkpoint: ...
             self.remove_keys_starting_with(state_dict, 'projection_head')
 
-            torch.save(ckpt, ckpt_path)
-            print(f"Modified checkpoint saved to: {ckpt_path}")
         elif self.cfg.method == "byol": # all of the heads will be initialized randomly
+            # 2 backbones: backbone and momentum_backbone
             self.remove_keys_starting_with(state_dict, 'projection_head')
-            self.remove_keys_starting_with(state_dict, 'momentum_projector')
             self.remove_keys_starting_with(state_dict, 'prediction_head')
             self.remove_keys_starting_with(state_dict, 'teacher_projection')
             
             self.rename_keys_starting_with(state_dict, 'teacher_backbone', 'momentum_backbone') # rename
+            # classifier, projector, momentum_projector, predictor will be initialized randomly
             
+        elif self.cfg.method == "dino":
+            # 2 backbones: student_backbone and momentum_backbone
+            self.remove_keys_starting_with(state_dict, 'projection_head')
+            self.remove_keys_starting_with(state_dict, 'student_projection')
+            self.remove_keys_starting_with(state_dict, 'criterion')
+           
+            self.rename_keys_starting_with(state_dict, 'student_backbone', 'momentum_backbone')
+            # classifier, head, momentum_head, dino_loss_func will be initialized randomly
             
-            torch.save(ckpt, ckpt_path)
-            print(f"Modified checkpoint saved to: {ckpt_path}")
         else:
             raise NotImplementedError(f"Method {self.cfg.method} not implemented yet.")
+        
+        torch.save(ckpt, ckpt_path)
+        print(f"Modified checkpoint saved to: {ckpt_path}")
 
     def process_checkpoint(self):
         """Main function to process the checkpoint by duplicating and resetting heads."""
