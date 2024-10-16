3af17bb1db7548700e4366f6573a45b48dcebc09
kargibora <kargibora@gmail.com>
2024-10-16 22:50:50 +0200

    update on sh

--- bash/run_grid_pretraining.sh
+++ bash/run_grid_pretraining.sh
@@ -9,31 +9,37 @@ METHOD_NAMES=("simclr" "byol" "dino" "ressl" "mocov3")
 RESNET_MODEL="resnet50"
 
 BATCH_SIZE=256
-EPOCHS=400
+EPOCHS=4
 GRID_SEARCH_EPOCHS=$((EPOCHS / 4))
-PRETRAINED=("True")
-CHECKPOINT_DIR="/graphics/scratch2/students/saritas/checkpoints"
+PRETRAINED=("False")
+CHECKPOINT_DIR="/graphics/scratch2/students/kargibo/experiments"
 
-# List of datasets to run
-# DATASETS=("pathmnist" "dermamnist" "octmnist", "pneumoniamnist" "retinamnist" "breastmnist" "bloodmnist") # Replace with your actual dataset names
-DATASETS=("organamnist" "organcmnist" "organsmnist" "tissuemnist") 
+# Get the dataset from argument
+DATASET=$1
 
-# Loop over datasets
-for DATASET in "${DATASETS[@]}"
+if [ -z "$DATASET" ]; then
+    echo "Please provide a dataset as an argument"
+    exit 1
+fi
+
+for PRETRAIN in "${PRETRAINED[@]}"
 do
-    echo "Running experiments for dataset: $DATASET"
-
     for METHOD_NAME in "${METHOD_NAMES[@]}"
     do
-        for PRETRAIN in "${PRETRAINED[@]}"
-        do
-            python main_solo.py --config-path $PRETRAIN_MEDMNIST_PATH --config-name $METHOD_NAME  \
+            # Check if the RESNET_MODEL starts with "vit"
+            if [[ $RESNET_MODEL == vit* ]]; then
+                CONFIG_NAME="${METHOD_NAME}_vit"
+            else
+                CONFIG_NAME=$METHOD_NAME
+            fi
+
+            echo "Running $CONFIG_NAME on $DATASET with $RESNET_MODEL backbone"
+            python main_solo.py --config-path $PRETRAIN_MEDMNIST_PATH --config-name $CONFIG_NAME  \
                 data=$DATASET optimizer.batch_size=$BATCH_SIZE max_epochs=$EPOCHS \
                 backbone.name=$RESNET_MODEL backbone.kwargs.pretrained=$PRETRAIN \
-                grid_search.enabled=True grid_search.linear_max_epochs=100 \
+                grid_search.enabled=True grid_search.linear_max_epochs=1 \
                 grid_search.pretrain_max_epochs=$GRID_SEARCH_EPOCHS \
                 name="${DATASET}-${METHOD_NAME}-${RESNET_MODEL}-epochs-${EPOCHS}-pretrained-${PRETRAIN}-batch_size-${BATCH_SIZE}" \
                 checkpoint.dir=$CHECKPOINT_DIR
-        done
     done
 done
